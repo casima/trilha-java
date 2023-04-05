b9e43cb9d86ea5ac40a751eab7906c364a057fba
casima <brunoabcx@gmail.com>
2023-04-05 17:44:37 -0300

    Operadores em Java

--- operadores/src/App.java
+++ operadores/src/App.java
@@ -12,14 +12,15 @@ public class App {
         char sexo = 'M';
         boolean doarOrgao = false;
         Date dataNascimento = new Date();
-
+        System.out.println("Fim Atribuição\n\n");
+        
         //Aritmétricos
         double soma = 10.5 + 60.4; //Operador de soma faz a concatenação numa String
         int subtracao = 40 - 20;
         int multiplicacao = 19 * 3;
         int divisao = 15 / 5;
         int modulo = 15 % 4; //retorna o resto da divisão
-        double resultado = (10.5 * 30.5) + (9 / 9);
+        double operacaoMatematica = (10.5 * 30.5) + (9 / 9);
 
         String concatenacao = "?";
         concatenacao = 1+1+1+"1";
@@ -29,8 +30,27 @@ public class App {
         System.out.println(concatenacao+"\n");
 
         concatenacao = 1+"1"+1+"1"; 
-        System.out.println(concatenacao+"\n");
+        System.out.println(concatenacao);
+        System.out.println("Fim Aritmétricos\n\n");
+       
 
 
+        //Unários
+        int resultado = +1;
+        System.out.println(resultado);
+ 
+        resultado--;
+        System.out.println(resultado);
+ 
+        resultado++;
+        System.out.println(resultado);
+ 
+        resultado = -resultado;
+        System.out.println(resultado);
+ 
+        boolean success = false;
+        System.out.println(success);
+        System.out.println(!success);
+        System.out.println("Fim Unários\n\n");
     }
 }
