cee218591160f0b72605808e0783a4999ac8fb12
casima <brunoabcx@gmail.com>
2023-04-05 16:02:35 -0300

    Java implementando int de um short, com cast.

--- tipos-variaveis/src/App.java
+++ tipos-variaveis/src/App.java
@@ -1,5 +1,11 @@
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Bora começar!");
+        //Número trocando seu tipo
+        short numeroCurto = 1; //2 bytes
+        int numeroNormal = numeroCurto; //4 bytes
+        short numeroCurto2 = (short) numeroNormal;  //cast
+        System.out.println("\n\nNumero:" + numeroCurto2 +"\n\n");
+        
     }
 }
