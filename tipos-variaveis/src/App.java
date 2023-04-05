649564b59a09149aee43b81e9390945d0cf59928
casima <brunoabcx@gmail.com>
2023-04-05 16:06:35 -0300

    Java: Uso de cast

--- tipos-variaveis/src/App.java
+++ tipos-variaveis/src/App.java
@@ -4,7 +4,7 @@ public class App {
         //Número trocando seu tipo
         short numeroCurto = 1; //2 bytes
         int numeroNormal = numeroCurto; //4 bytes
-        short numeroCurto2 = (short) numeroNormal;  //cast
+        short numeroCurto2 = (short) numeroNormal;  //cast: pega um tipo mais ambrangente e muda para uma precisão menor
         System.out.println("\n\nNumero:" + numeroCurto2 +"\n\n");
         
     }
