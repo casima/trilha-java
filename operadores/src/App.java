830ee50884165da3e4f3ce1c6e85dee06bff8f7a
casima <brunoabcx@gmail.com>
2023-04-06 10:12:05 -0300

    Operadores em Java

--- operadores/src/App.java
+++ operadores/src/App.java
@@ -49,9 +49,9 @@ public class App {
         System.out.println(resultado);
  
         boolean success = false;
-        System.out.println(success);
+        System.out.println(success +" (declarada ela é false)");
         System.out.println(!success +" (em memória ela é true)");//em memória ela é true
-        System.out.println(success);
+        System.out.println(success +" (continua false)");
         success = !success;
         System.out.println(success);
         System.out.println("Fim Unários\n\n");
