4b86aabb2b1c2fe71b018234b11733eb70bcbae2
casima <brunoabcx@gmail.com>
2023-04-06 10:16:30 -0300

    Operadores em Java

--- operadores/src/App.java
+++ operadores/src/App.java
@@ -50,7 +50,7 @@ public class App {
  
         boolean success = false;
         System.out.println(success +" (valor declarado false)");
-        System.out.println(!success +" (em memória é false)");//em memória é false
+        System.out.println(!success +" (na memória: false)");// mas saída: true
         System.out.println(success +" (false)");
         success = !success;
         System.out.println(success);
